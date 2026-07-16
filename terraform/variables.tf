f85e05be4eee70eb33de266f5b63b81d2357eba5
Shouvik Jha <58849426+jhashouvik@users.noreply.github.com>
2026-07-16 18:08:07 +0530

    Update diagram artifacts via Archly

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -15,116 +15,40 @@ variable "tenant_id" {
 }
 
 # >>> archly:group:vnet1 >>>
-variable "vnet1_address_space" {
-  description = "Address space for VNet 'Workload VNet'"
-  type        = string
-  default     = "10.0.0.0/16"
-}
-# <<< archly:group:vnet1 <<<
-
-# >>> archly:group:appgw_subnet >>>
-variable "appgw_subnet_address_prefix" {
-  description = "Address prefix for subnet 'App Gateway Subnet' (must fall within the VNet's address space)"
-  type        = string
-  default     = "10.0.1.0/24"
-}
-# <<< archly:group:appgw_subnet <<<
-
-# >>> archly:group:apps_subnet >>>
-variable "apps_subnet_address_prefix" {
-  description = "Address prefix for subnet 'Container Apps Subnet' (must fall within the VNet's address space)"
+variable "vnet1_address_prefix" {
+  description = "Address prefix for subnet 'VNet' (must fall within the VNet's address space)"
   type        = string
   default     = "10.0.2.0/24"
 }
-# <<< archly:group:apps_subnet <<<
+# <<< archly:group:vnet1 <<<
 
-# >>> archly:group:pe_subnet >>>
-variable "pe_subnet_address_prefix" {
-  description = "Address prefix for subnet 'Private Endpoint Subnet' (must fall within the VNet's address space)"
-  type        = string
-  default     = "10.0.3.0/24"
-}
-# <<< archly:group:pe_subnet <<<
 
-# >>> archly:group:data_services >>>
-variable "data_services_address_space" {
-  description = "Address space for VNet 'Managed Data Services'"
-  type        = string
-  default     = "10.0.0.0/16"
-}
-# <<< archly:group:data_services <<<
 
-# >>> archly:group:security_ops >>>
-variable "security_ops_address_space" {
-  description = "Address space for VNet 'Security & Operations'"
-  type        = string
-  default     = "10.0.0.0/16"
-}
-# <<< archly:group:security_ops <<<
 
-# >>> archly:group:platform_controls >>>
-variable "platform_controls_address_space" {
-  description = "Address space for VNet 'Platform Controls'"
-  type        = string
-  default     = "10.0.0.0/16"
-}
-# <<< archly:group:platform_controls <<<
 
-# >>> archly:node:appgw1 >>>
-variable "appgw1_name" {
-  description = "Name for azurerm_application_gateway"
-  type        = string
-}
-# <<< archly:node:appgw1 <<<
 
-# >>> archly:node:apps1 >>>
-variable "apps1_container_image" {
-  description = "Container image to deploy, e.g. myrepo/app:latest"
-  type        = string
-}
-# <<< archly:node:apps1 <<<
 
-# >>> archly:node:mi1 >>>
-variable "mi1_name" {
-  description = "Name for azurerm_managed"
-  type        = string
-}
-# <<< archly:node:mi1 <<<
 
-# >>> archly:node:sqlpe1 >>>
-variable "sqlpe1_name" {
-  description = "Name for azurerm_private_endpoint"
-  type        = string
-}
-# <<< archly:node:sqlpe1 <<<
 
-# >>> archly:node:redispe1 >>>
-variable "redispe1_name" {
-  description = "Name for azurerm_private_endpoint"
-  type        = string
-}
-# <<< archly:node:redispe1 <<<
 
-# >>> archly:node:sbpe1 >>>
-variable "sbpe1_name" {
-  description = "Name for azurerm_private_endpoint"
-  type        = string
-}
-# <<< archly:node:sbpe1 <<<
 
-# >>> archly:node:blobpe1 >>>
-variable "blobpe1_name" {
-  description = "Name for azurerm_private_endpoint"
-  type        = string
-}
-# <<< archly:node:blobpe1 <<<
 
-# >>> archly:node:kvpe1 >>>
-variable "kvpe1_name" {
-  description = "Name for azurerm_private_endpoint"
-  type        = string
-}
-# <<< archly:node:kvpe1 <<<
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # >>> archly:node:sql1 >>>
 variable "sql1_administrator_login" {
@@ -140,33 +64,11 @@ variable "sql1_administrator_password" {
 }
 # <<< archly:node:sql1 <<<
 
-# >>> archly:node:sql2 >>>
-variable "sql2_administrator_login" {
-  description = "Administrator login"
-  type        = string
-  default     = "sqladminuser"
-}
 
-variable "sql2_administrator_password" {
-  description = "Administrator password -- supply via TF_VAR_sql2_administrator_password, never commit"
-  type        = string
-  sensitive   = true
-}
-# <<< archly:node:sql2 <<<
 
-# >>> archly:node:dns1 >>>
-variable "dns1_name" {
-  description = "Name for azurerm_private"
-  type        = string
-}
-# <<< archly:node:dns1 <<<
 
-# >>> archly:node:logs1 >>>
-variable "logs1_name" {
-  description = "Name for azurerm_logging"
-  type        = string
-}
-# <<< archly:node:logs1 <<<
+
+
 
 # >>> archly:node:azure_policy >>>
 variable "azure_policy_name" {
@@ -175,23 +77,155 @@ variable "azure_policy_name" {
 }
 # <<< archly:node:azure_policy <<<
 
-# >>> archly:node:microsoft_entra_id >>>
-variable "microsoft_entra_id_name" {
-  description = "Name for azurerm_identity_center"
+# >>> archly:group:azurefirewallsubnet >>>
+variable "azurefirewallsubnet_address_prefix" {
+  description = "Address prefix for subnet 'AzureFirewallSubnet' (must fall within the VNet's address space)"
+  type        = string
+  default     = "10.0.6.0/24"
+}
+# <<< archly:group:azurefirewallsubnet <<<
+
+# >>> archly:group:mgmt_group >>>
+variable "mgmt_group_address_space" {
+  description = "Address space for VNet 'Management Group'"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+# <<< archly:group:mgmt_group <<<
+
+# >>> archly:group:subnet1 >>>
+variable "subnet1_address_prefix" {
+  description = "Address prefix for subnet 'Application Subnet' (must fall within the VNet's address space)"
+  type        = string
+  default     = "10.0.3.0/24"
+}
+# <<< archly:group:subnet1 <<<
+
+# >>> archly:group:subnet2 >>>
+variable "subnet2_address_prefix" {
+  description = "Address prefix for subnet 'Data Subnet' (must fall within the VNet's address space)"
+  type        = string
+  default     = "10.0.4.0/24"
+}
+# <<< archly:group:subnet2 <<<
+
+# >>> archly:group:subnet3 >>>
+variable "subnet3_address_prefix" {
+  description = "Address prefix for subnet 'Private Endpoint Subnet' (must fall within the VNet's address space)"
+  type        = string
+  default     = "10.0.5.0/24"
+}
+# <<< archly:group:subnet3 <<<
+
+# >>> archly:group:subscription >>>
+variable "subscription_address_prefix" {
+  description = "Address prefix for subnet 'Subscription' (must fall within the VNet's address space)"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+# <<< archly:group:subscription <<<
+
+# >>> archly:node:agw1 >>>
+variable "agw1_name" {
+  description = "Name for azurerm_application_gateway"
   type        = string
 }
-# <<< archly:node:microsoft_entra_id <<<
+# <<< archly:node:agw1 <<<
 
-# >>> archly:node:defender_for_cloud >>>
-variable "defender_for_cloud_name" {
-  description = "Name for azurerm_guardduty"
-  type        = string
-}
-# <<< archly:node:defender_for_cloud <<<
-
-# >>> archly:node:sql2_pe >>>
-variable "sql2_pe_name" {
+# >>> archly:node:blob1_pe >>>
+variable "blob1_pe_name" {
   description = "Name for azurerm_private_endpoint"
   type        = string
 }
-# <<< archly:node:sql2_pe <<<
+# <<< archly:node:blob1_pe <<<
+
+# >>> archly:node:containerapps1 >>>
+variable "containerapps1_container_image" {
+  description = "Container image to deploy, e.g. myrepo/app:latest"
+  type        = string
+}
+# <<< archly:node:containerapps1 <<<
+
+# >>> archly:node:defender >>>
+variable "defender_name" {
+  description = "Name for azurerm_security_hub"
+  type        = string
+}
+# <<< archly:node:defender <<<
+
+# >>> archly:node:keyvault1 >>>
+variable "keyvault1_name" {
+  description = "Name for azurerm_manager"
+  type        = string
+}
+# <<< archly:node:keyvault1 <<<
+
+# >>> archly:node:keyvault1_pe >>>
+variable "keyvault1_pe_name" {
+  description = "Name for azurerm_private_endpoint"
+  type        = string
+}
+# <<< archly:node:keyvault1_pe <<<
+
+# >>> archly:node:managed_identity >>>
+variable "managed_identity_name" {
+  description = "Name for azurerm_managed"
+  type        = string
+}
+# <<< archly:node:managed_identity <<<
+
+# >>> archly:node:private_dns >>>
+variable "private_dns_name" {
+  description = "Name for azurerm_private"
+  type        = string
+}
+# <<< archly:node:private_dns <<<
+
+# >>> archly:node:rbac >>>
+variable "rbac_name" {
+  description = "Name for azurerm_identity_center"
+  type        = string
+}
+# <<< archly:node:rbac <<<
+
+# >>> archly:node:redis1_pe >>>
+variable "redis1_pe_name" {
+  description = "Name for azurerm_private_endpoint"
+  type        = string
+}
+# <<< archly:node:redis1_pe <<<
+
+# >>> archly:node:servicebus1_pe >>>
+variable "servicebus1_pe_name" {
+  description = "Name for azurerm_private_endpoint"
+  type        = string
+}
+# <<< archly:node:servicebus1_pe <<<
+
+# >>> archly:node:sql1_pe >>>
+variable "sql1_pe_name" {
+  description = "Name for azurerm_private_endpoint"
+  type        = string
+}
+# <<< archly:node:sql1_pe <<<
+
+# >>> archly:node:sql_failover >>>
+variable "sql_failover_administrator_login" {
+  description = "Administrator login"
+  type        = string
+  default     = "sqladminuser"
+}
+
+variable "sql_failover_administrator_password" {
+  description = "Administrator password -- supply via TF_VAR_sql_failover_administrator_password, never commit"
+  type        = string
+  sensitive   = true
+}
+# <<< archly:node:sql_failover <<<
+
+# >>> archly:node:sql_failover_pe >>>
+variable "sql_failover_pe_name" {
+  description = "Name for azurerm_private_endpoint"
+  type        = string
+}
+# <<< archly:node:sql_failover_pe <<<
